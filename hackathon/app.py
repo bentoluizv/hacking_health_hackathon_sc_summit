9cdf2186b0baced311f7fe98143a20619143636b
bentoluizv <bentoluizv@gmail.com>
2024-11-12 00:46:46 -0300

    add /cadastro e /pacientes/id html routes

--- hackathon/app.py
+++ hackathon/app.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, Request
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
 
 from hackathon.api import (
     atendimento,
@@ -10,6 +14,9 @@ from hackathon.api import (
     paciente,
     triagem,
 )
+from hackathon.db import get_session
+from hackathon.models import PacienteOrm
+from hackathon.utils.to_tb import paciente_orm_to_model
 
 app = FastAPI()
 
@@ -22,6 +29,31 @@ templates = Jinja2Templates(directory='templates')
 @app.get('/', response_class=HTMLResponse)
 async def index_page(request: Request):
     return templates.TemplateResponse(request=request, name='index.html')
+
+
+@app.get('/cadastro', response_class=HTMLResponse)
+async def cadastro_paciente(request: Request):
+    return templates.TemplateResponse(request=request, name='cadastro.html')
+
+
+@app.get('/pacientes/{id}', response_class=HTMLResponse)
+async def paciente_page(
+    request: Request, session: Annotated[Session, Depends(get_session)]
+):
+    paciente_orm = session.get(PacienteOrm, id)
+
+    if not paciente_orm:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Paciente não encontrado'
+        )
+
+    paciente_model = paciente_orm_to_model(paciente_orm)
+
+    return templates.TemplateResponse(
+        request=request,
+        name='paciente.html',
+        context={'paciente': paciente_model},
+    )
 
 
 app.include_router(atendimento.router)
