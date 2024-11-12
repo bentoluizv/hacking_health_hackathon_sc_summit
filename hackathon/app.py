c416dc7d51dfb12ec1ea20e79d2d2987640752b0
bentoluizv <bentoluizv@gmail.com>
2024-11-11 22:49:00 -0300

    add route get / to render index.html

--- hackathon/app.py
+++ hackathon/app.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from hackathon.api import (
     atendimento,
@@ -9,6 +12,16 @@ from hackathon.api import (
 )
 
 app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/', response_class=HTMLResponse)
+async def index_page(request: Request):
+    return templates.TemplateResponse(request=request, name='index.html')
 
 
 app.include_router(atendimento.router)
