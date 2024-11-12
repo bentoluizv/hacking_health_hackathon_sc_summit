e7f45b74f1a0eb21149f8ea51e42c979e8805d5c
bentoluizv <bentoluizv@gmail.com>
2024-11-12 11:46:56 -0300

    remove unused files and fix index

--- hackathon/app.py
+++ hackathon/app.py
@@ -1,18 +1,11 @@
-from http import HTTPStatus
-from typing import Annotated
-
-from fastapi import Depends, FastAPI, HTTPException, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session
 
 from hackathon.api import (
-    paciente,
     triagem,
 )
-from hackathon.db import get_session
-from hackathon.models import PacienteOrm
 
 app = FastAPI()
 
@@ -22,30 +15,22 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
 
 
+@app.get('/', response_class=RedirectResponse)
+def index_page():
+    return '/triagem'
+
+
 @app.get(
     '/triagem',
     response_class=HTMLResponse,
 )
 async def triagem_page(
-    paciente_id: str,
     request: Request,
-    session: Annotated[Session, Depends(get_session)],
 ):
-    paciente_orm = session.get(PacienteOrm, paciente_id)
-
-    if not paciente_orm:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='Paciente não encontrado'
-        )
-
     return templates.TemplateResponse(
         request=request,
-        name='triagem.html',
-        context={
-            'paciente': paciente_orm,
-        },
+        name='index.html',
     )
 
 
-app.include_router(paciente.router)
 app.include_router(triagem.router)
